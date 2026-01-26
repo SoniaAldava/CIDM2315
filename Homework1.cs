d85c848386e61d606e44d18fe8b28921b004fd18
SoniaAldava <saldava1@buffs.wtamu.edu>
2026-01-25 18:39:49 -0600

    Update Homework1.cs
    Added comments to homework1

--- Homework1.cs
+++ Homework1.cs
@@ -1,12 +1,13 @@
 ﻿namespace Homework1;
 
-class Program
+class Program 
 {
-    static void Main(string[] args)
+    static void Main(string[] args) 
     {
-        double X = 2.5;
-        double Y = 3.3;
-        double Z = 4 * X * X + 3 * Y;
-        Console.WriteLine($"The value of Z is: "+Z);
+        double X = 2.5; //Set the value of X and defining the variable as a double.
+        double Y = 3.3; //Set the value of Y and defining the variable as a double.
+        double Z = 4 * X * X + 3 * Y; //Set the Value of Z. Set 4x2 + 3Y as 4*X*X + 3*Y.
+        Console.WriteLine($"The value of Z is: "+Z); //Print the value of z by placing $ symbol before the string so that Z can be placed in the ().
     }
+
 }
